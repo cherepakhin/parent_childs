91615ad6b7b61a2b6cf1faa446ed6d4bcfcf3194
Черепахин Василий <vasi.che@gmail.com>
2023-06-06 21:56:13 +0500

    Setup project

---
+++ src/test/java/ru/perm/v/parent_childs/SimpleTest.java
@@ -0,0 +1,12 @@
+package ru.perm.v.parent_childs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    void simple() {
+        assertEquals(1,1);
+    }
+}
